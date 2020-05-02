4b019bff5c5552138d7b869c5297e67d7c0539ae
Alfio Puglisi <alfio.puglisi@inaf.it>
2020-05-02 12:42:15 +0200

    Added server-only main program

---
+++ quickgui/examples/motor/main_motor_server.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+A simple main program that binds together
+a task and a GUI using multiprocessing.
+In addition the motor has a TCP server interface
+'''
+from quickgui.examples.motor.motor_dispatch_task import task
+
+from quickgui.framework import get_server, start
+
+host = ''
+port = 3333
+
+start(task=task, task_servers = get_server(host, port))
